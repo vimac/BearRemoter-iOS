abb9d743cef46f77757bf2ef032ace2d76e624a4
vimac <vifix.mac@gmail.com>
2015-08-15 20:59:43 +0800

    Initial Commit

---
+++ BearRemoter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BearRemoter
+//
+//  Created by Mac on 8/15/15.
+//  Copyright (c) 2015 vifix.cn. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
